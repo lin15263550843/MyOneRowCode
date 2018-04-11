e4cbde766f3796c570d05fcf0d300c965cc5b483
林洪东 <1625125333@qq.com>
2018-04-11 09:45:41 +0800

    Menu弹出+Toast提示

--- app/src/main/java/com/example/lhd/myonerowcode/MainActivity.java
+++ app/src/main/java/com/example/lhd/myonerowcode/MainActivity.java
@@ -3,9 +3,32 @@ package com.example.lhd.myonerowcode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "debug级别的信息";
+    private static final String TAG = "MainActivity";
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.main_menu_item_1: Toast.makeText(this, "添加", Toast.LENGTH_SHORT).show();break;
+            case R.id.main_menu_item_2: Toast.makeText(this, "移除", Toast.LENGTH_SHORT).show();break;
+            case R.id.main_menu_item_3: Toast.makeText(this, "Item3", Toast.LENGTH_SHORT).show();break;
+            default:
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +39,16 @@ public class MainActivity extends AppCompatActivity {
         Log.v("MainActivityVVV", "VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV");
         Log.d("MainActivityDDD", "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
         Log.w("MainActivityWWW", "WWWWWwWWWWWwWWWWWwWWWWWwWWWWWwWWWWWwWWW");
+
+        Button mainButton1 = findViewById(R.id.main_button_1);
+        mainButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "我是弹窗，你咬我呀~", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
+
 }
---
+++ app/src/main/java/com/example/lhd/myonerowcode/homeActivity.java
@@ -0,0 +1,15 @@
+package com.example.lhd.myonerowcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class homeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_layout);
+
+
+    }
+}
