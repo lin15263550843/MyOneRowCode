d421fbb9c88bb017874b107ba4cbbd96dcbcb7a5
林洪东 <1625125333@qq.com>
2018-08-29 17:21:45 +0800

    全局 Context

---
+++ app/src/main/java/com/example/lhd/myonerowcode/service/MyApplication.java
@@ -0,0 +1,30 @@
+package com.example.lhd.myonerowcode.service;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by dhc on 2018/8/29.
+ * 自定义 Application
+ * <p>
+ * Android 提供了一个 Application 类，每当应用程序启动的时候，系统就会自动将这个类进行初始化。
+ * 而我们可以定制一个自己的 Application 类，以便于管理程序内一些全局的状态信息，比如说全局 Context。
+ * <p>
+ * 调用 MyApplication.getContext() 方法获取 Context 对象。
+ */
+
+public class MyApplication extends Application {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    // 返回全局的 Context
+    public static Context getContext() {
+        return context;
+    }
+}
